c1bd3dade2459628c936c2bfc013907174b0aeab
mkoryakov <mnkoryakov@yandex.ru>
2017-01-20 00:21:24 +0300

    Написана функция форматирования цены и автоматические тесты для неё.

--- format_price.py
+++ format_price.py
@@ -1,5 +1,20 @@
+from argparse import ArgumentParser
+
+
 def format_price(price):
-    pass
+    try:
+        stirng_price = str(price)
+        float_price = float(stirng_price)
+        comma_separator_price = '{0:,.2f}'.format(float_price)
+        return comma_separator_price.replace(',', ' ')
+    except ValueError:
+        return None
+
 
 if __name__ == '__main__':
-    pass
+    parser = ArgumentParser(prog='format_price',
+                            description='Скрипт, форматирующий цену товара')
+    parser.add_argument('price', help='цена товара')
+    unformatted_price = parser.parse_args().price
+    formatted_price = format_price(unformatted_price)
+    print(formatted_price)
--- tests.py
+++ tests.py
@@ -1,1 +1,37 @@
-# TODO place your tests here
+import unittest
+
+from format_price import format_price
+
+
+class FormatPriceTestCase(unittest.TestCase):
+    def test_int_type(self):
+        price = format_price(2014)
+        self.assertEqual(price, '2 014.00')
+
+    def test_float_type(self):
+        price = format_price(2014.0)
+        self.assertEqual(price, '2 014.00')
+
+    def test_true_string_type(self):
+        price = format_price('2014')
+        self.assertEqual(price, '2 014.00')
+
+    def test_false_string_type(self):
+        price1 = format_price('2014,')
+        price2 = format_price('2014.00.')
+        price3 = format_price('A2014.0')
+        self.assertIsNone(price1)
+        self.assertIsNone(price2)
+        self.assertIsNone(price3)
+
+    def test_list_type(self):
+        price = format_price([2014])
+        self.assertIsNone(price)
+
+    def test_dict_type(self):
+        price = format_price({'price': 2014})
+        self.assertIsNone(price)
+
+
+if __name__ == '__main__':
+    unittest.main()
